be6bcc2a45b90aefebe4b6f737759a41988e7b57
Newport <sarah.newport@ferguson.com>
2016-07-28 19:52:43 +0000

    adjusted style format

--- nCurses/window.cpp
+++ nCurses/window.cpp
@@ -2,94 +2,100 @@
 #include <unistd.h>
 
 // for drawing borders around each window (top, middle, bottom)
-void draw_borders(WINDOW *screen) {
-  int x, y, i;
+void draw_borders(WINDOW* screen)
+{
+    int x, y, i;
 
-  getmaxyx(screen, y, x);
+    getmaxyx(screen, y, x);
 
-  // the 4 corners
-  mvwprintw(screen, 0, 0, "*");
-  mvwprintw(screen, y - 1, 0, "*");
-  mvwprintw(screen, 0, x - 1, "*");
-  mvwprintw(screen, y - 1, x - 1, "*");
+    // the 4 corners
+    mvwprintw(screen, 0, 0, "*");
+    mvwprintw(screen, y - 1, 0, "*");
+    mvwprintw(screen, 0, x - 1, "*");
+    mvwprintw(screen, y - 1, x - 1, "*");
 
-  // sides of the borders
-  for (i = 1; i < (y - 1); i++) {
-    mvwprintw(screen, i, 0, "|");
-    mvwprintw(screen, i, x - 1, "|");
-  }
-
-  // top and bottom
-  for (i = 1; i < (x - 1); i++) {
-    mvwprintw(screen, 0, i, "*");
-    mvwprintw(screen, y - 1, i, "*");
-  }
-}
-
-int main(int argc, char *argv[]) {
-  int parent_x, parent_y, new_x, new_y;
-  int middle_size = 12;
-  int bottom_size = 3;
-
-  initscr();
-  curs_set(1);
-
-  // set up the initial windows
-  getmaxyx(stdscr, parent_y, parent_x);
-
-  WINDOW *top = newwin(parent_y - middle_size, parent_x, 0, 0);
-  WINDOW *middle = newwin(middle_size - bottom_size, parent_x, parent_y - middle_size, 0);
-  WINDOW *bottom = newwin(bottom_size, parent_x, parent_y - bottom_size, 0);
-
-    // run through and call borders function for each window
-  draw_borders(top);
-  draw_borders(middle);
-  draw_borders(bottom);
-  
-  // window colors
-  start_color();
-  init_pair(1, COLOR_WHITE, COLOR_BLACK);
-  bkgd(COLOR_PAIR(1));
-  wbkgd(stdscr, COLOR_PAIR(1));
-  wbkgd(top, COLOR_PAIR(1));
-  wbkgd(middle, COLOR_PAIR(1));
-  wbkgd(bottom, COLOR_PAIR(1));
-  refresh();
-  
-
-    // this will be for resizing still needs work
-   while(1) {
-     getmaxyx(stdscr, new_y, new_x);
-
-    if (new_y != parent_y || new_x != parent_x) {
-      parent_x = new_x;
-      parent_y = new_y;
-
-      wresize(top, new_y - bottom_size, new_x);
-      wresize(bottom, bottom_size, new_x);
-      mvwin(bottom, new_y - bottom_size, 0);
-
-      wclear(stdscr);
-      wclear(top);
-      wclear(middle);
-      wclear(bottom);
-
-      draw_borders(top);
-      draw_borders(middle);
-      draw_borders(bottom);
+    // sides of the borders
+    for (i = 1; i < (y - 1); i++)
+    {
+        mvwprintw(screen, i, 0, "|");
+        mvwprintw(screen, i, x - 1, "|");
     }
 
-    // print out to each window
-    mvwprintw(top, 1, 1, "TOP WINDOW");
-    mvwprintw(middle, 1, 1, "MIDDLE WINDOW");
-    mvwprintw(bottom, 1, 1, "TEXT INPUT WINDOW");
+    // top and bottom
+    for (i = 1; i < (x - 1); i++)
+    {
+        mvwprintw(screen, 0, i, "*");
+        mvwprintw(screen, y - 1, i, "*");
+    }
+}
 
-    // refresh each window
-    wrefresh(top);
-    wrefresh(middle);
-    wrefresh(bottom);
-   }
+int main(int argc, char* argv[])
+{
+    int parent_x, parent_y, new_x, new_y;
+    int middle_size = 12;
+    int bottom_size = 3;
 
-  endwin();
-  return 0;
+    initscr();
+
+    // set up the initial windows
+    getmaxyx(stdscr, parent_y, parent_x);
+
+    // defining the three windows
+    WINDOW* top = newwin(parent_y - middle_size, parent_x, 0, 0);
+    WINDOW* middle = newwin(middle_size - bottom_size, parent_x, parent_y - middle_size, 0);
+    WINDOW* bottom = newwin(bottom_size, parent_x, parent_y - bottom_size, 0);
+
+    // color section
+    start_color();
+    init_pair(1, COLOR_WHITE, COLOR_BLACK);
+
+    bkgdset(COLOR_PAIR(1));
+    wbkgdset(top, COLOR_PAIR(1));
+    wbkgdset(middle, COLOR_PAIR(1));
+    wbkgdset(bottom, COLOR_PAIR(1));
+    
+    // run through and call borders function for each window
+    draw_borders(top);
+    draw_borders(middle);
+    draw_borders(bottom);
+
+    // this will be for resizing still needs some work to perfect
+    while (1)
+    {
+        getmaxyx(stdscr, new_y, new_x);
+
+        if (new_y != parent_y || new_x != parent_x)
+        {
+            parent_x = new_x;
+            parent_y = new_y;
+
+            wresize(top, new_y - middle_size, new_x);
+            wresize(middle, middle_size - bottom_size, new_x);
+            wresize(bottom, bottom_size, new_x);
+            mvwin(bottom, new_y - bottom_size, 0);
+
+            wclear(stdscr);
+            wclear(top);
+            wclear(middle);
+            wclear(bottom);
+
+            draw_borders(top);
+            draw_borders(middle);
+            draw_borders(bottom);
+        }
+
+        // print out to each window
+        mvwprintw(top, 1, 1, "GRAPHICS WINDOW");
+        mvwprintw(middle, 1, 1, "TEXT DESCRIPTION WINDOW");
+        mvwprintw(bottom, 1, 1, "TEXT INPUT WINDOW");
+
+        // refresh each window
+        wrefresh(stdscr);
+        wrefresh(top);
+        wrefresh(middle);
+        wrefresh(bottom);
+    }
+
+    endwin();
+    return 0;
 }
---
+++ nCurses/window2.cpp
@@ -0,0 +1,92 @@
+#include <ncurses.h>
+#include <unistd.h>
+
+// function for drawing borders around each window
+void draw_borders(WINDOW *screen) {
+  int x, y, i;
+
+  getmaxyx(screen, y, x);
+
+  // the 4 corners of the borders
+  mvwprintw(screen, 0, 0, "*");
+  mvwprintw(screen, y - 1, 0, "*");
+  mvwprintw(screen, 0, x - 1, "*");
+  mvwprintw(screen, y - 1, x - 1, "*");
+
+  // sides of the borders
+  for (i = 1; i < (y - 1); i++) {
+    mvwprintw(screen, i, 0, "|");
+    mvwprintw(screen, i, x - 1, "|");
+  }
+
+  // top and bottom of the borders
+  for (i = 1; i < (x - 1); i++) {
+    mvwprintw(screen, 0, i, "*");
+    mvwprintw(screen, y - 1, i, "*");
+  }
+}
+
+int main(int argc, char *argv[]) {
+  int parent_x, parent_y, new_x, new_y;
+  int middle_size = 12;
+  int bottom_size = 3;
+
+  initscr();
+  curs_set(1);
+
+  // set up the initial windows
+  getmaxyx(stdscr, parent_y, parent_x);
+
+  WINDOW *top = newwin(parent_y - middle_size, parent_x, 0, 0);
+  WINDOW *middle = newwin(middle_size - bottom_size, parent_x, parent_y - middle_size, 0);
+  WINDOW *bottom = newwin(bottom_size, parent_x, parent_y - bottom_size, 0);
+
+  // run through and call borders function for each window
+  draw_borders(top);
+  draw_borders(middle);
+  draw_borders(bottom);
+  
+  start_color();
+  init_pair(1, COLOR_WHITE, COLOR_BLACK);
+  bkgd(COLOR_PAIR(1));
+  wbkgd(stdscr, COLOR_PAIR(1));
+  wbkgd(top, COLOR_PAIR(1));
+  wbkgd(middle, COLOR_PAIR(1));
+  wbkgd(bottom, COLOR_PAIR(1));
+  
+  while(1) {
+     getmaxyx(stdscr, new_y, new_x);
+
+    if (new_y != parent_y || new_x != parent_x) {
+      parent_x = new_x;
+      parent_y = new_y;
+
+      wresize(top, new_y - bottom_size, new_x);
+      wresize(bottom, bottom_size, new_x);
+      mvwin(bottom, new_y - bottom_size, 0);
+
+      wclear(stdscr);
+      wclear(top);
+      wclear(middle);
+      wclear(bottom);
+
+      draw_borders(top);
+      draw_borders(middle);
+      draw_borders(bottom);
+    }
+    
+  
+   mvwprintw(top, 1, 1, "GRAPHICS WINDOW");
+    mvwprintw(middle, 1, 1, "TEXT DESCRIPTION WINDOW");
+    mvwprintw(bottom, 1, 1, "TEXT INPUT WINDOW");
+    
+    wrefresh(stdscr);
+    wrefresh(top);
+    wrefresh(middle);
+    wrefresh(bottom);
+    
+  }
+  
+  endwin();
+  return 0;
+}

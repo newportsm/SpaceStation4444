70362084a815bdb24acb35121f3c1d63b6891229
Newport <sarah.newport@ferguson.com>
2016-07-29 14:03:42 +0000

    updated style

--- ASCII/graphics.cpp
+++ ASCII/graphics.cpp
@@ -2,43 +2,45 @@
 #include <fstream>
 #include <string>
 
+using namespace std;
+
 // get file contents
-std::string getFileContents (std::ifstream&);          
+string getFileContents (ifstream&);          
 
 int main(int argc, char *argv[])
 {
     // open file with file path
-    std::ifstream Title ("graphics/title.txt");
+    ifstream Title ("graphics/title.txt");
 
     // get file contents and place into ART
-    std::string OpeningTitle = getFileContents (Title);
+    string OpeningTitle = getFileContents (Title);
     
     //print contents to screen
-    std::cout << OpeningTitle << std::endl;               
+    cout << OpeningTitle << endl;               
 
     // close file
     Title.close ();
     
-    std::ifstream Space ("graphics/spaceship1.txt");
-    std::string SpaceShip = getFileContents (Space);
-    std::cout << SpaceShip << std::endl;               
+    ifstream Space ("graphics/spaceship1.txt");
+    string SpaceShip = getFileContents (Space);
+    cout << SpaceShip << endl;               
     Space.close ();  
 
     return 0;
 }
 
-std::string getFileContents (std::ifstream& File)
+string getFileContents (ifstream& File)
 {
     // all lines into string
-    std::string Lines = "";        
+    string Lines = "";        
     
     // while file exists
     if (File)                      
     {
 	while (File.good ())
 	{
-	    std::string TempLine;                  
-	    std::getline (File , TempLine);        
+	    string TempLine;                  
+	    getline (File , TempLine);        
 	    TempLine += "\n";                      
 	    
 	    Lines += TempLine;     
